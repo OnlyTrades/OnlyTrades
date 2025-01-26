067cb50600f0c42c8d1a1faf520378ee763fc3b2
windytiany <Tlalocayotl@proton.me>
2025-01-26 11:03:25 -0500

    front-end

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
+from datetime import datetime
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from pathlib import Path
-import time
-
-data = {}
-min_times = {}
-max_times = {}
     
 def load_csv_data(csv_file):
-    print(csv_file)
     df = pd.read_csv(csv_file)
-    print(df.head())
-    df['timestamp'] = pd.to_datetime(df['timestamp'], format='%H:%M:%S.%f').dt.time
+    df['timestamp'] = pd.to_datetime(df['timestamp'], format='%H:%M:%S')
     min_time = df['timestamp'].min()
     max_time = df['timestamp'].max()
     
     return df, min_time, max_time
 
+@st.cache_data
 def load_all_data():
-    dataset_path = "./TrainingData"
+    data = {}
+    min_times = {}
+    max_times = {}
+    dataset_path = "./TestData"
     stocks = ["A", "B", "C", "D", "E"]
-    periods = [idx for idx in range(1, 16)]
+    periods = [idx for idx in range(16, 21)]
     for stock in stocks:
         data[stock] = {}
         min_times[stock] = {}
@@ -38,48 +36,106 @@ def load_all_data():
                 if str(csv_file.stem)!=f"market_data_{stock}_0":
                     continue
                 data[stock][period], min_times[stock][period], max_times[stock][period] = load_csv_data(csv_file)
+    
+    return data, min_times, max_times
 
 
-def fetch_data(data, min_time, max_time):
+def fetch_data(data, min_times, max_times):
     stock = st.session_state.stock
     period = st.session_state.period
-    return data[stock][period], min_time[stock][period], max_time[stock][period]
+    return data[stock][period], min_times[stock][period], max_times[stock][period]
          
+hide_decoration_bar_style = '''
+    <style>
+        header {visibility: hidden;}
+        body {
+            background-color: #241E3A;
+            color: #C84E11;
+            margin-toop: 0;
+            padding-top: 0;
+        }
 
-st.title('OnlyTrades')
+        # /* Ensure the main content stretches across the full width /
+        # .stApp {
+        #     width: 100% !important;
+        # }
 
-with st.spinner('Loading data...'):
-    load_all_data()
-    print(data)
-st.success("Done!")
+        / Ensure background of the entire Streamlit app is styled /
+        .stApp {
+            background-color: #241E3A;
+        }
 
-stock_col, period_col, time_col = st.columns(3)
+        / Apply text color to Markdown, text, and other Streamlit text components /
+        .stMarkdown, .stText, .stTextInput, .stButton, .stSlider, .stRadio, .stCheckbox, .stFileUploader, .stSelectbox, .stMultiselect {
+            color: #C84E11 !important;
+        }
+
+        / Update the color for all headers /
+        h1, h2, h3, h4, h5, h6, p {
+            color: #C84E11 !important;
+        }
+
+        / For Streamlit buttons, widgets, sliders, checkboxes, etc. /
+        .stButton>button, .stSlider>div, .stRadio>div, .stCheckbox>div, .stSelectbox>div, .stMultiselect>div {
+            color: #C84E11 !important;
+        }
+
+        / Targeting links inside Streamlit for consistent color */
+        .css-1v0mbdj a, .css-1v0mbdj a:visited {
+            color: #C84E11 !important;
+        }
+
+        .centered-text {
+            font-size: 50px;
+            color: #C84E11;
+            text-align: center;
+        }
+        .st-emotion-cache-yw8pof {
+            padding-top: 0;
+        }
+    </style>
+    <hr style='margin: 0; border: 20px solid #C84E11; width: 100%;' />
+    <div class="centered-text">OnlyTrades</div>
+'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+
+data, min_times, max_times = load_all_data()
+
+stock_col, time_col = st.columns(2)
 
 # init
-st.session_state.stock = "A"
-st.session_state.period = 1
-st.session_state.display_data = data["A"][1]
-st.session_state.min_time = min_times["A"][1]
-st.session_state.max_time = max_times["A"][1]
-st.session_state.start_time = str(st.session_state.min_time)
-st.session_state.end_time = str(st.session_state.max_time)
-st.session_state.show_bid_price = True
-st.session_state.show_ask_price = True
-st.session_state.show_bid_volume = True
-st.session_state.show_ask_volume = True
+if st.session_state.get("stock") is None:
+    st.session_state.stock = "A"
+if st.session_state.get("period") is None:
+    st.session_state.period = 16
+stock = st.session_state.stock
+period = st.session_state.period
+st.session_state.display_data = data[stock][period]
+st.session_state.min_time = min_times[stock][period]
+st.session_state.max_time = max_times[stock][period]
+if st.session_state.get("start_time") is None:
+    st.session_state.start_time = st.session_state.min_time.strftime(format='%H:%M:%S')
+if st.session_state.get("end_time") is None:
+    st.session_state.end_time = st.session_state.max_time.strftime(format='%H:%M:%S')
+if st.session_state.get("displayed_price") is None:
+    st.session_state.displayed_price = ["bidPrice", "askPrice"]
+if st.session_state.get("displayed_volume") is None:
+    st.session_state.displayed_volume = ["bidVolume", "askVolume"]
 
 def on_content_change():
     st.session_state.display_data, st.session_state.min_time, st.session_state.max_time = fetch_data(data, min_times, max_times)
+    st.session_state.start_time = st.session_state.min_time.strftime(format='%H:%M:%S')
+    st.session_state.end_time = st.session_state.max_time.strftime(format='%H:%M:%S')
     
 def on_time_change():
     current_data, _, _ = fetch_data(data, min_times, max_times)
     try:
-        start_time = time.strptime(st.session_state.start_time, format='%H:%M:%S.%f')
+        start_time = datetime.strptime(st.session_state.start_time, format='%H:%M:%S')
     except ValueError:
         start_time = st.session_state.min_time
         st.session_state.start_time = str(st.session_state.min_time)
     try:
-        end_time = time.strptime(st.session_state.end_time, format='%H:%M:%S.%f')
+        end_time = datetime.strptime(st.session_state.end_time, format='%H:%M:%S')
     except ValueError:
         end_time = st.session_state.max_time
         st.session_state.end_time = str(st.session_state.max_time)
@@ -89,8 +145,18 @@ def on_time_change():
         and current_data["timestamp"] <= end_time
     )
     
+    
 def on_display_change():
-    pass
+    st.session_state.displayed_price = []
+    st.session_state.displayed_volume = []
+    if st.session_state.show_bid_price:
+        st.session_state.displayed_price.append("bidPrice")
+    if st.session_state.show_ask_price:
+        st.session_state.displayed_price.append("askPrice")
+    if st.session_state.show_bid_volume:
+        st.session_state.displayed_volume.append("bidVolume")
+    if st.session_state.show_ask_volume:
+        st.session_state.displayed_volume.append("askVolume")
     
 with stock_col:
     stock_option = st.selectbox(
@@ -99,11 +165,9 @@ with stock_col:
         key="stock",
         on_change=on_content_change
     )
-
-with period_col:
     period_option = st.selectbox(
         label='Period',
-        options=list(range(1, 16)),  # List of numbers from 1 to 15
+        options=list(range(16, 21)),  # List of numbers from 1 to 15
         key="period",
         on_change=on_content_change
     )
@@ -122,55 +186,66 @@ with time_col:
     submit_botton = st.button(
         label="Confirm",
         on_click=on_time_change
+
     )
 
-st.title("Bid Price & Ask Price")
-st.line_chart(
-    st.session_state.display_data,
-    x="timestamp",
-    y=["bidPrice", "askPrice"],
+st.subheader("Bid Price & Ask Price")
+st.checkbox("Bid Price", key="show_bid_price", value=True, on_change=on_display_change)
+st.checkbox("Ask Price", key="show_ask_price", value=True, on_change=on_display_change)
+price_figure = go.Figure()
+# Add traces for each line
+for column in st.session_state.displayed_price:
+    price_figure.add_trace(
+        go.Scatter(
+            x=st.session_state.display_data["timestamp"], 
+            y=st.session_state.display_data[column], 
+            mode='lines', 
+            name=column
+        )
+    )
+# Customize layout
+price_figure.update_layout(
+    title="Price Chart",
+    xaxis_title="Time",
+    yaxis_title="Price",
+    legend_title="Lines",
+    template="plotly_white",
+    hovermode="x",
+    xaxis=dict(
+        tickformat='%H:%M:%S',
+        dtick=600000
+    )
 )
-#fig1 = go.Figure()
-#fig1.add_trace(go.Scatter(
-#    x=st.session_state.display_data['timestamp'], 
-#    y=st.session_state.display_data['bidPrice'], 
-#    mode='lines', 
-#    name='Bid Price',
-#    hovertemplate='Index: %{x}<br>Bid Price: %{y}<extra></extra>', 
-#    hoverinfo='none'
-#))
-## Update layout
-#fig1.update_layout(
-#    title="Bid Price over Index",
-#    xaxis_title="Index",
-#    yaxis_title="Bid Price",
-#    hovermode="x"
-#)
-## Show the interactive plot in Streamlit
-#    
-#fig = go.Figure()
-## Add the bidPrice data as a line plot
-#fig.add_trace(go.Scatter(
-#    x=st.session_state.display_data['timestamp'], 
-#    y=st.session_state.display_data['bidVolume'], 
-#    mode='lines', 
-#    name='Bid Volume',
-#    text=st.session_state.display_data['bidVolume'],  # This is the value shown when hovering
-#    hovertemplate='Index: %{x}<br>Bid Volume: %{y}<extra></extra>'  # Custom tooltip
-#))
-## Update layout
-#fig.update_layout(
-#    title="Bid Volume over Index",
-#    xaxis_title="Time",
-#    yaxis_title="Bid Volume",
-#    hovermode="x"  # Ensures hovering works well
-#)
-## Show the interactive plot in Streamlit
-   
-    
-#c1, c2 = st.columns(2)
-#with c1:
-#        st.plotly_chart(fig1)
-#with c2:
-#    st.plotly_chart(fig)
-    
+# Display the chart in Streamlit
+st.plotly_chart(price_figure)
+
+st.subheader("Bid Volume & Ask Volume")
+st.checkbox("Bid Volume", key="show_bid_volume", value=True, on_change=on_display_change)
+st.checkbox("Ask Volume", key="show_ask_volume", value=True, on_change=on_display_change)
+volume_figure = go.Figure()
+# Add traces for each line
+for column in st.session_state.displayed_volume:
+    volume_figure.add_trace(
+        go.Scatter(
+            x=st.session_state.display_data["timestamp"], 
+            y=st.session_state.display_data[column], 
+            mode='lines', 
+            name=column
+        )
+    )
+    # Customize layout
+volume_figure.update_layout(
+    title="Price Chart",
+    xaxis_title="Time",
+    yaxis_title="Volume",
+    legend_title="Lines",
+    template="plotly_white",
+    hovermode="x",
+    xaxis=dict(
+        tickformat='%H:%M:%S',
+        dtick=600000
+    )
+)
+# Display the chart in Streamlit
+st.plotly_chart(volume_figure)
+
